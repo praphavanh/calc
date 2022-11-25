76fdd045be46cca92cb6e8867cddc07470204e00
Zakk <praphavanh@gmail.com>
2022-11-25 13:43:43 -0600

    operations js

--- script.js
+++ script.js
@@ -50,3 +50,10 @@ numberButtons.forEach(button => {
         calculator.updateDisplay()
     })
 })
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
